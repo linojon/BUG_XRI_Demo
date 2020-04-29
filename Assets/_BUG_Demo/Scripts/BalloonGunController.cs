757c42c00a6c70bbda17730a5600d0c71031191a
Jonathan Linowes <linojon@gmail.com>
2020-04-29 16:36:59 -0400

    initial commit

---
+++ Assets/_BUG_Demo/Scripts/BalloonGunController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonGunController : MonoBehaviour
+{
+    public Transform gunTip;
+    public GameObject balloonPrefab;
+    public float startScale = 0.1f;
+    public float growRate = 1.5f;
+    public float maxScale = 1.0f;
+    public float shootVelocity = 3.0f;
+
+    private GameObject balloon;
+    private Rigidbody rb;
+
+    private void Update()
+    {
+        if (balloon != null)
+        {
+            GrowBalloon();
+        }
+    }
+
+    public void CreateBalloon()
+    {
+        balloon = Instantiate(balloonPrefab, gunTip.transform);
+        balloon.transform.localScale = new Vector3(startScale, startScale, startScale);
+        rb = balloon.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    public void ReleaseBalloon()
+    {
+        balloon.transform.parent = null;
+        rb.isKinematic = false;
+        rb.velocity = gunTip.forward * shootVelocity;
+
+        GameObject.Destroy(balloon, 10f);
+        balloon = null;
+    }
+
+    public void GrowBalloon()
+    {
+        if (balloon.transform.localScale.x > maxScale)
+            return;
+
+        Vector3 changeScale = balloon.transform.localScale * growRate * Time.deltaTime;
+        balloon.transform.localScale += changeScale;
+}
+}
---
+++ Assets/_BUG_Demo/Scripts/ResettableTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResettableTransform : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    public void ResetTransform()
+    {
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+    }
+}
